490865e34ab17e3404dcb05dbbe7cf790277d878
nicolaslarrondo <nicolaslarrondo@liceovvh.cl>
2024-04-15 09:51:35 -0400

    ok

--- Fundamento JS/bucles/ejercicio-1/scrip.js
+++ Fundamento JS/bucles/ejercicio-1/scrip.js
@@ -2,12 +2,78 @@
 
 function cinco(){
 
-    let numero = 0;
+  let numero = []
+  let numero2 = 0;
+  let pare = 50
+ 
+  var i = 0
 
-
-    while( numero == 100){
-       numero += 5;
-
-       document.getElementById("hola").innerHTML = numero
-    }
+  while(i<=100){
+    numero2 += 5
+    numero.push(numero2)
+    i++;
+    
+   if(i == pare ){
+   numero.push(`<br>`)
+   pare = pare + 50
+   }
+  }
+  
+  document.getElementById("hola").innerHTML  = numero;
+ 
 }
+
+function uno_a_dies(){
+
+  let numero = []
+  let numero2 = 0
+  let total= 0
+  var i = 0
+
+  while(i<=9){
+    
+    numero2++
+    numero.push(numero2 + `+`)
+    i++
+    
+     total += numero2
+    
+
+  }
+
+  document.getElementById("m").innerHTML = numero + `=${total}`
+  
+}
+
+function pares(){
+ 
+  let pare = [2]
+  let pares2 =2
+  let i = 0
+
+  while(i<=8){
+    pares2 += 2
+    pare.push(pares2)
+    i++
+    
+  }
+  document.getElementById("p").innerHTML = pare
+
+}
+
+
+  function compararNumeros() {
+    var numeros = [];
+    var numeroMayor;
+    
+    for (var i = 0; i < 5; i++) {
+        var numero = prompt("Ingresa el número " + (i+1));
+        numeros.push(Number(numero));
+    
+        if (i === 0 || numero > numeroMayor) {
+            numeroMayor = numero;
+        }
+    }
+
+   alert("el numero mayor es: " + numeroMayor)
+  }
