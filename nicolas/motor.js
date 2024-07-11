a92e7c2c1ed7c570d21846c8326c270b413c6384
nicolaslarrondo <nicolaslarrondo@liceovvh.cl>
2024-07-11 11:15:58 -0400

    hola

--- nicolas/motor.js
+++ nicolas/motor.js
@@ -3,7 +3,30 @@
 
 
 
-function motor_1(){
+
+  
+
+document.getElementById('boton').addEventListener('click', function() {
+  var inpute = document.getElementById("p").value
+  const resultado = document.getElementById('lista');
+  
+  const p_1 = document.createElement('li');
+  p_1.textContent = inpute// Si `this` se refiere al botón, esto no tiene mucho sentido, normalmente sería `input.value`
+
+  resultado.appendChild(p_1);
+
+  console.log(this);
+});
+
+
+
+
+
+
+
+
+
+/*function motor_1(){
 
 var l = document.getElementById("p").value
 const p_1 = document.getElementById("lista")
@@ -16,4 +39,4 @@ p_1.appendChild(p_2)
 
 
 
-}
+}*/
--- nicolas/nicolas2/el_motor.js
+++ nicolas/nicolas2/el_motor.js
@@ -2,10 +2,14 @@
 
 
 
-var puntos = 0;
+puntos = 0;
+document.getElementById('miBoton').addEventListener('click', () => {
+// En este caso, `this` no se refiere al botón, sino al contexto léxico superior.
 
-function mi_funcion(){
+this.puntos += 1;
 
-puntos += 1;
-document.getElementById("p").innerHTML = `el puntaje es${puntos}`
-}
+
+    document.getElementById('miBoton').innerHTML = `el puntaje es${puntos}`;
+
+
+});
